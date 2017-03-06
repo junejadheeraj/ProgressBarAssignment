63dd3ed7a4609025d9713885ba66fd2ad59fd800
DheerajJuneja <dheeraj.juneja@DDC1-L-5465NLS.dir.svc.accenture.com>
2017-03-06 11:41:16 +0530

    service updated

--- src/webapp/scripts/app.js
+++ src/webapp/scripts/app.js
@@ -43,28 +43,3 @@ app.controller('BarController', ['$scope','calcService', function($scope,calcSer
 		}
 	}  
 }]);
-
-app.service("calcService",function()
-{
-	this.compute = function(barValue,addValue)
-	{
-		this.barValue = barValue;
-		this.addValue = addValue;
-		if(this.barValue + this.addValue < 0)
-	  {
-		  this.barValue = 0;	 
-	  }else
-	  {
-		  this.barValue = this.barValue + this.addValue;	 
-		  if(this.barValue<=100)
-			{
-				this.myBarClass = "progress-bar-info";		 
-			} else if(this.barValue>100)
-			{
-				this.myBarClass = "progress-bar-danger";		
-			}
-			
-	  }  
-		
-	}
-});
